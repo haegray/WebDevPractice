52c5ea99e96dba3a9969cffc38d43dbc9ecccf2a
haegray <anaisgray.ag@gmail.com>
2022-02-27 23:36:23 -0500

    made search bar work

--- wiki/encyclopedia/views.py
+++ wiki/encyclopedia/views.py
@@ -6,9 +6,24 @@ import markdown
 
 
 def index(request):
-    return render(request, "encyclopedia/index.html", {
-        "entries": util.list_entries()
-    })
+    if request.method == "POST":
+        title = util.get_entry(request.POST.get('q'))
+
+        if title is not None:
+            return render(request, "encyclopedia/show_page.html", {
+                "title": markdown.markdown(title)
+            })
+        else:
+            title =  request.POST.get('q')
+            entries = util.list_entries()
+            entries = [entry for entry in entries if title.lower() in entry.lower()]
+            return render(request, "encyclopedia/index.html", {
+            "entries": entries
+        })
+    else:
+        return render(request, "encyclopedia/index.html", {
+            "entries": util.list_entries()
+        })
 
 def TITLE(request, title):
     title = util.get_entry(title)
