3bb9e63468598632a43506fcb63b0f1a5b63c6d3
haegray <anaisgray.ag@gmail.com>
2022-03-09 19:48:42 -0500

    Added bidding

--- commerce/auctions/views.py
+++ commerce/auctions/views.py
@@ -32,8 +32,6 @@ class ListingForm(ModelForm):
         title = self.cleaned_data.get('title')
         current_price = self.cleaned_data.get('current_price')
         owned_by = self.cleaned_data.get('owned_by')
-        description = self.cleaned_data.get('description')
-        photo = self.cleaned_data.get('photo')
 
  
         # conditions to be met for the username length
@@ -52,6 +50,54 @@ class ListingForm(ModelForm):
         # return any errors if found
         return self.cleaned_data
 
+class BidForm(ModelForm):
+    
+    class Meta:
+        # write the name of models for which the form is made
+        model = Bid    
+        fields = '__all__'
+        widgets = {
+            'bidder': forms.HiddenInput(), "listed_item": forms.HiddenInput()
+        }
+        # Custom fields
+
+
+    def __init__(self, *args, **kwargs):
+         self.user = kwargs.pop('user', None)
+         self.listed_item = kwargs.pop('listed_item', None)
+         super(BidForm, self).__init__(*args, **kwargs)
+         #self.fields['bidder'].queryset = user
+         #self.fields['listed_item'] = listing
+    # this function will be used for the validation
+    def clean(self):
+ 
+        # data from the form is fetched using super function
+        super(BidForm, self).clean()
+         
+        # extract the username and text field from the data
+        
+        self.cleaned_data['bidder'] = self.user
+        del self._errors['bidder']
+
+        self.cleaned_data['listed_item'] = self.listed_item
+        del self._errors['listed_item']
+
+        # conditions to be met for the username length
+        if self.cleaned_data['bid'] < self.listed_item.current_price:
+            print("price")
+            self._errors['bid'] = self.error_class([
+                'Bid must be above current price.'])
+
+        if self.listed_item.bids.filter(bid=self.cleaned_data['bid'], bidder=self.cleaned_data['bidder']).exists():
+            self._errors['bid'] = self.error_class([
+                'Cannot make the same bid on one listing.' 
+            ])
+
+        print(self.cleaned_data)
+        print(self.errors)
+        # return any errors if found
+        return self.cleaned_data
+
 def index(request):
     listings = Listing.objects.all()
     return render(request, "auctions/index.html", {
@@ -60,17 +106,52 @@ def index(request):
 
 def listing(request, title):
     listing = Listing.objects.get(title = title)
-    bids = [bid for bid in listing.bids.all()]
-   
-    return render(request, "auctions/listing.html", {
-        "listing": listing,
-        "bids": bids
-    })
+    user = User.objects.get(username = request.user.username)
+    bids = Bid.objects.filter(listed_item=listing)
+
+    if request.method == "POST":
+        form = BidForm(request.POST, user=user, listed_item=listing)
+
+
+        if form.is_valid():
+            bid = form.cleaned_data['bid']
+            instance = form.save(commit=False)
+            instance.bidder = request.user
+            instance.listed_item = listing
+            instance.save()
+
+            listing.current_price = bid
+            listing.save()
+            return render(request, "auctions/listing.html", {
+                "form": BidForm(user=request.user, listed_item = listing),
+                "listing": listing,
+                "bids": bids,
+                "current_user": User.objects.get(username=request.user)
+            })
+        else:
+            print("Nope")
+            return render(request, "auctions/listing.html", {
+                "form": form,
+                "listing": listing,
+                "bids": bids,
+                "current_user": request.user
+            })
+    else:
+        print("Listing owned by ", listing.owned_by)
+        print("user ", request.user)
+        return render(request, "auctions/listing.html", {
+            "form": BidForm(user=request.user, listed_item = listing),
+            "listing": listing,
+            "bids": bids,
+            "current_user": User.objects.get(username=request.user)
+        })
+
+
 
 def add_listing(request):
     if request.method == "POST":
 
-            form = ListingForm(request.POST, request.FILES,user=request.user)
+            form = ListingForm(request.POST, request.FILES, user=request.user)
             if form.is_valid():
                 print("Hello")
                 title = form.cleaned_data['title']
