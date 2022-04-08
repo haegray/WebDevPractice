5dfc7c752d3fca8dadd3aaa99552e756c136716b
haegray <anaisgray.ag@gmail.com>
2022-04-08 16:17:38 -0400

    Added reply functionality and made archiveable emails

--- mail/mail/mail/admin.py
+++ mail/mail/mail/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
 
 # Register your models here.
+
+from .models import Email, User
+# Register your models here.
+
+class EmailAdmin(admin.ModelAdmin):
+    list_display = ("user","sender","show_recipients","subject","body","timestamp","read","archived")
+
+    def show_recipients(self, obj):
+            return "\n".join([a.username for a in obj.recipients.all()])
+    
+admin.site.register(Email, EmailAdmin)
--- mail/mail/mail/static/mail/inbox.js
+++ mail/mail/mail/static/mail/inbox.js
@@ -5,7 +5,6 @@ function sleep(ms) {
 document.addEventListener('DOMContentLoaded', function() {
 
     // By default, load the inbox
-  
   // Use buttons to toggle between views
   document.querySelector('#inbox').addEventListener('click', () => load_mailbox('inbox'));
   document.querySelector('#sent').addEventListener('click', () => load_mailbox('sent'));
@@ -14,8 +13,11 @@ document.addEventListener('DOMContentLoaded', function() {
   load_mailbox("inbox");
 });
 
-function compose_email() {
+function compose_email(email_now) {
 
+
+  document.querySelector('#compose-recipients').disabled = false;
+  document.querySelector('#compose-subject').disabled = false;
   // Show compose view and hide other views
   document.querySelector('#emails-view').style.display = 'none';
   document.querySelector('#compose-view').style.display = 'block';
@@ -24,34 +26,52 @@ function compose_email() {
   document.querySelector('#compose-recipients').value = '';
   document.querySelector('#compose-subject').value = '';
   document.querySelector('#compose-body').value = '';
+ 
+  if(email_now !== "" && email_now.subject !== undefined && email_now.subject !== ""){
+    document.querySelector('#compose-recipients').value = email_now.sender;
+    if(!email_now.subject.startsWith('Re:')){
+      document.querySelector('#compose-subject').value = "Re: " + email_now.subject;
+    } else {
+      document.querySelector('#compose-subject').value = email_now.subject;
+    }
+    document.querySelector('#compose-body').value = email_now.body;
+
+    document.querySelector('#compose-recipients').disabled = true;
+    document.querySelector('#compose-subject').disabled = true;
+  }
 
   //Get email and send email
     document.querySelector('#compose-form').onsubmit = function() {
 
+      
       const recipients = document.querySelector('#compose-recipients').value;
       const subject = document.querySelector('#compose-subject').value;
-      const body = document.querySelector('#compose-body').value;
-
+      const body = "\n On " + new Date() + " " + document.querySelector('#compose-sender').value + " wrote: " + document.querySelector('#compose-body').value + "\n";
+     
+      
       fetch('/emails', {
         method: 'POST',
         body: JSON.stringify({
             recipients: recipients,
             subject: subject,
-            body: body
+            body: body,
+            read: false
         })
       })
-      .then(response => response.json())
+      .then(response => response.text())
       .then(result => {
-          // Print result
-          console.log(result);
-      })
-      .then(load_mailbox("sent"));
+        console.log(result);
+        load_mailbox("sent");
+      });
+
+      
       return false;
     }
     
 }
 
 function show_emails(mailbox, emails) {
+
 
   var styles = `
   * {
@@ -91,7 +111,7 @@ function show_emails(mailbox, emails) {
     element.id = 'email-' + email_now.id;
 
     
-    if(i%2 === 0){
+    if(email_now.read===true){
       element.style.backgroundColor = "#DCDCDC";
     } 
 
@@ -149,11 +169,17 @@ function show_emails(mailbox, emails) {
     document.querySelector('#emails-view').append(element);
     element.addEventListener('click', ()=> view_email(mailbox, email_now));
     
-    
   }
 }
 
 function view_email(mailbox, email_now){
+  str = '/emails/' + email_now.id
+  fetch(str, {
+    method: 'PUT',
+    body: JSON.stringify({
+        read: true
+    })
+  });
 
   document.querySelector('#compose-view').style.display = 'none';
 
@@ -183,11 +209,52 @@ function view_email(mailbox, email_now){
     element.appendChild(recipient);
   }
 
+  var button = document.createElement('input'),
+    archive = "archive";
+
+    if(email_now.archived === true){
+      archive = "unarchive";
+    } 
+
+  button.type  = 'button';
+  button.value = archive;
+  button.addEventListener('click', () => {
+    let str = '/emails/' + email_now.id;
+    console.log(str);
+    fetch(str, {
+      method: 'PUT',
+      body: JSON.stringify({
+          archived: !email_now.archived
+      })})
+      .then(result => {
+        console.log(result);
+        load_mailbox("inbox")
+      });
+  });
+
+  element.appendChild(button);
+
+  if(mailbox === "inbox"){
+
+    var button_send = document.createElement('input'),
+    br_send = document.createElement('br');
+
+    button_send.type  = 'button';
+    button_send.value = "reply";
+    button_send.addEventListener('click', () => {
+      compose_email(email_now);
+    });
+
+    element.appendChild(button_send);
+    element.appendChild(br_send);
+  }
+
+  
+
   let email_body = document.createElement("p");
   email_body.textContent = email_now.body;
   element.appendChild(email_body);
   document.querySelector('#emails-view').append(element);
-
 }
 
 
